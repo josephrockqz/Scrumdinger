7c7e71773c0be3dccd460c9610bbfa86182f7470
Joseph Rock <josephrockqz@gmail.com>
2022-11-11 12:32:12 -0500

    Prototyped Meeting View

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Joseph Rock on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
